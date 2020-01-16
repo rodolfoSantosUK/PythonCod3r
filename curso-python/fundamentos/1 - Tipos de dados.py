2574fc90fa775c441e95a4f9850befaf87f8d5d5
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-01-16 12:50:48 -0300

    Tipos de dados, variaveis e comentarios

---
+++ curso-python/fundamentos/1 - Tipos de dados.py
@@ -0,0 +1,7 @@
+
+print(True)
+print(False)
+print(1.2)
+print([1, 2, 3])
+print({'nome' : 'Pedro', 'idade' : 22 })  #Dicionário
+print(None) 
---
+++ curso-python/fundamentos/3 - Comentarios/comentarios.py
@@ -0,0 +1,10 @@
+salario = 3450.0
+despesas = 4000
+
+"""
+ testando comentario com mais de 
+ uma 
+ linha 
+ """
+
+ ctrl + /  para comentar codigo
