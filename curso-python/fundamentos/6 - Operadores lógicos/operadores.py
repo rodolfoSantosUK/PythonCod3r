0a23289f4a16e4bc731b3a75a5a1d846568ed181
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-01-16 15:03:01 -0300

    Operadores

---
+++ curso-python/fundamentos/6 - Operadores lógicos/operadores.py
@@ -0,0 +1,36 @@
+# tabela verdade do and
+
+print(True and True)
+print(True and False) # Se tiver ao menos um falso na expressao retorna falso
+print(False and True)
+
+print(False and False)
+
+
+# tabela verdade do Or
+
+print(True or True)
+print(True or False)
+print(False or True) # Se tiver ao menos um true na expressão retorna true
+print(False or False)
+
+# Tabela verdade do NOR (Ou um ou outro)
+
+print('True != True')
+print(True != True)
+
+print('True != False')
+print(True != False)
+
+print('False != True')
+print(False != True)
+
+print('False != False')
+print(False != False)
+
+
+# Operação de negação 
+print (not True) # Negando uma expressão True
+print (not False) # Negando uma expressão False
+
+print(not 0)
