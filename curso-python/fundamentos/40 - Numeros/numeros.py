eb5821922d5f07c2db196e8f7237e2965d22e893
Rodolfo Santos <santoseuro2018@gmail.com>
2020-01-18 20:15:52 -0500

    Trabalhando com numeros

---
+++ curso-python/fundamentos/40 - Numeros/numeros.py
@@ -0,0 +1,15 @@
+from decimal import Decimal, getcontext
+
+print(dir())
+
+print(dir(__builtins__))
+
+Decimal(1) / Decimal(7)
+
+# MUDANDO O CONTEXTO DE PRECISÃO DE CASAS DECIMAIS
+
+getcontext().prec = 4
+
+print(Decimal(1) / Decimal(4))
+
+print(Decimal.max(Decimal(1), Decimal(7)))
