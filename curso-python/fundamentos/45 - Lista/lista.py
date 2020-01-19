23f9e22397c0949f563e1e31cc962b3a431f31a0
Rodolfo Santos <santoseuro2018@gmail.com>
2020-01-19 14:40:53 -0500

    usando tuplas

---
+++ curso-python/fundamentos/45 - Lista/lista.py
@@ -0,0 +1,18 @@
+lista = []
+type(lista)
+ 
+
+lista = [1,5,'Rebeca', 'Guilherme', 3.1415]
+lista.index('Guilherme')
+
+lista[2]
+
+print(1 in lista)
+
+
+lista_nomes = ['Ana','Lia','Rui','Paulo','Dani']
+
+lista_nomes[1:3]
+lista_nomes[1:-1]
+lista_nomes[1:] # A partir do indice 1 em diante
+lista_nomes[::-2] # Pula de 2 em 2 de trás pra frente
