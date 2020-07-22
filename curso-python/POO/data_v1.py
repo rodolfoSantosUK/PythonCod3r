106c50a759f4e2705da629dbada63c554928b74d
Rodolfo Santos <santoseuro2018@gmail.com>
2020-07-22 20:51:42 -0300

    Trabalhando com construtor, deve-se se ter apenas uns contrutor em python, porem voce pode criar construtores nomeados, possibilitando assim instanciar uma classe de forma diferente.

--- curso-python/POO/data_v1.py
+++ curso-python/POO/data_v1.py
@@ -1,23 +1,18 @@
 class Data:
     
-    def to_str(self):
-       return  f'{self.dia}/{self.mes}/{self.ano}'
-
+    def __init__(self, dia=1, mes=9, ano=1970):
+        self.dia = dia
+        self.mes = mes 
+        self.ano = ano
+        
     def __str__ (self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
-d1 = Data()
-d1.dia = 5
-d1.mes = 12
-d1.ano = 2019
 
-print(d1.to_str())
+d1 = Data(5, 12, 2019)
+print(d1)
 
-d2 = Data()
-d2.dia = 10
-d2.mes = 4
-d2.ano = 1986
-
+d2 = Data(ano =2022)
 print(d2)
 
 
