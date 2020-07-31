46647a1da6e02fddb8fbbac2aea9caf022a9f6db
Rodolfo Santos <santoseuro2018@gmail.com>
2020-07-31 15:21:38 -0300

    Dando continuidade a orientação a objetos criamos uma classe produto que atraves dele é adicionada uma lista de tarefas.

--- curso-python/POO/todo_v1.py
+++ curso-python/POO/todo_v1.py
@@ -1,5 +1,25 @@
 from datetime import datetime
 
+
+class Projeto:
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+        
+    def add(self, descricao):
+        self.tarefas.append(Tarefa(descricao))
+            
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito ]         
+        
+    def procurar(self, descricao):
+        return [tarefa for tarefa in self.tarefas if tarefa.descricao == descricao][0]
+    
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())}  tarefa(s) pendente(s) )'
+        
+
 class Tarefa: 
     
     def __init__(self, descricao):
@@ -14,15 +34,16 @@ class Tarefa:
         return self.descricao + (' Concluida' if self.feito else '')
     
 def main():
-    casa = []
-    casa.append(Tarefa('Passar roupa'))
-    casa.append(Tarefa('Lavar prato'))
+    casa = Projeto('Tarefa de casa')
+    casa.add('Passar roupa')
+    casa.add('Lavar prato')
+    print(casa)
     
-    [tarefa.concluir() for tarefa in casa if tarefa.descricao == 'Lavar prato']
     
-    for tarefa in casa:
-        print(f'- {tarefa}')
-        
+    casa.procurar('Lavar prato').concluir()
+    for tarefa in casa.tarefas:
+        print(f' - {tarefa}')
+    print(casa)    
         
 if __name__  == '__main__':
     main()       
